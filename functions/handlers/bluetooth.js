995aaf556f4b36ee708022b7509bbf93a675dd20
lukecheng1998 <lukecheng1998@gmail.com>
2020-07-14 15:39:00 -0700

    sucessfully added post bluetooth device

--- functions/handlers/bluetooth.js
+++ functions/handlers/bluetooth.js
@@ -2,20 +2,16 @@ const { db } = require("../util/admin");
 const firebase = require("firebase");
 const config = require("../util/config");
 exports.postBluetoothDevice = (req, res) => {
-
-    const DeviceData = {
-        deviceId: req.body.deviceId,
-        time: req.body.date
-    }
-    db.doc(`/users/${req.user.email}`).collection('devices').get().then(doc => {
-        if (doc.exists) {
-            db.doc(`/users/${req.user.email}`).collection('devices').add(DeviceData);
-        }
-    }).then(() => {
-        console.log("Successfully Added Device.");
-    }).catch(err => {
-        return res.status(500).json({err:err.code})
+  const DeviceData = {
+    deviceId: req.body.deviceId,
+    time: new Date().toISOString()
+  };
+  console.log(DeviceData)
+  db.doc(`/users/${req.user.email}`).collection("devices").doc(DeviceData.deviceId).set(DeviceData)
+    .then(() => {
+      return res.status(200).json({ message: "Sucessfully added device" });
     })
-
-  
-}
+    .catch((err) => {
+      return res.status(500).json({ err: err.code });
+    });
+};
